09e1bec27f1c75558b25547b706cfec3794cc1d3
snoopyouaib <snoopyouaib@gmail.com>
2026-08-14 11:49:20 +0200

    splash 2.41

--- chrono-AMOLED-2.41/lib/splash/splash_pigteam.h
+++ chrono-AMOLED-2.41/lib/splash/splash_pigteam.h
@@ -3,9 +3,9 @@
 
 #include "lvgl.h"
 
-// Image splash PigTeam, 536x240, convertie depuis le format Piskel/
-// ARGB8888 d'origine (splash_amoled_pigteam.c fourni) vers RGB565
-// natif LVGL. Utilisation : lv_img_set_src(img_obj, &splash_pigteam_img);
+// Image splash PigTeam "Fairy Pig", 600x450 -- remplit tout le canevas
+// du 2.41 (12/08, remplace l'ancienne image 536x240 heritee du 1.91).
+// Utilisation : lv_img_set_src(img_obj, &splash_pigteam_img);
 extern const lv_img_dsc_t splash_pigteam_img;
 
 #endif
